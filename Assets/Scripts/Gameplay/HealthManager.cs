25757879a1d006710964e9f915d20eebd7ddbe19
AvynEbersold <48456012+AvynEbersold@users.noreply.github.com>
2023-04-16 18:19:23 -0400

    Add pistol model and colors to health bar

--- Assets/Scripts/Gameplay/HealthManager.cs
+++ Assets/Scripts/Gameplay/HealthManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class HealthManager : MonoBehaviour
@@ -17,6 +18,14 @@ public class HealthManager : MonoBehaviour
     public AudioClip deathSound;
     public bool takingDamage = false;
 
+    // healthbar color by number - each color is for 20% of the health bar
+    // ex. player is at 35% health, then should have medLowHealthColor healthbar
+    public Color32 highHealthColor;
+    public Color32 medHighHealthColor;
+    public Color32 medHealthColor;
+    public Color32 medLowHealthColor;
+    public Color32 lowHealthColor;
+
     private TextMeshProUGUI healthTextComponent;
     private RectTransform healthTextTransformComponent;
     private AudioSource audioSourceComponent;
@@ -24,6 +33,7 @@ public class HealthManager : MonoBehaviour
     private RectTransform healthBarTransformComponent;
     private float healthBarStartingWidth;
     private float healthBarStartingHeight;
+    private RawImage healthBarRawImage;
 
     // Start is called before the first frame update
     void Start()
@@ -34,21 +44,39 @@ public class HealthManager : MonoBehaviour
         healthBarTransformComponent = healthBar.GetComponent<RectTransform>();
         healthBarStartingWidth = healthBarTransformComponent.sizeDelta.x;
         healthBarStartingHeight = healthBarTransformComponent.sizeDelta.y;
+        healthBarRawImage = healthBar.GetComponent<RawImage>();
         audioSourceComponent = audioManager.GetComponent<AudioSource>();
         UpdateUI();
     }
 
     private void UpdateUI()
     {
+        // make the health bar be the right size, color, and alignment
+        float healthRatio = (float)playerCurrentHealth / (float)playerStartHealth;
+        Vector2 healthBarSize = new Vector2(healthRatio * healthBarStartingWidth, healthBarStartingHeight);
+        healthBarTransformComponent.sizeDelta = healthBarSize;
+        healthBarTransformComponent.localPosition = new Vector3((healthBarSize.x - healthBarStartingWidth) / 2, 0, 0);
+        if(healthRatio > 0.81)
+        {
+            healthBarRawImage.color = highHealthColor;
+        } else if (healthRatio > 0.61)
+        {
+            healthBarRawImage.color = medHighHealthColor;
+        } else if (healthRatio > 0.41)
+        {
+            healthBarRawImage.color = medHealthColor;
+        } else if (healthRatio > 0.21)
+        {
+            healthBarRawImage.color = medLowHealthColor;
+        } else
+        {
+            healthBarRawImage.color = lowHealthColor;
+        }
+
+        // show the correct health number in correct position
         string healthString = playerCurrentHealth.ToString();
         healthTextComponent.text = healthString;
-
-        Vector2 healthBarSize = new Vector2(((float)playerCurrentHealth / (float)playerStartHealth) * healthBarStartingWidth, 150);
-        healthBarTransformComponent.sizeDelta = healthBarSize;
-        healthBarTransformComponent.localPosition = new Vector3((healthBarSize.x - healthBarStartingWidth)/2, 0, 0);
-
         float healthTextXPos = -(healthBarStartingWidth / 2) + healthBarSize.x + healthTextOffset;
-
         healthTextTransformComponent.localPosition = new Vector3(healthTextXPos, 0, 0);
     }
 
